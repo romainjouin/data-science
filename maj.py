0b197543fac272ee2ca841266cc523fdedd24c2c
Romain Jouin <romain.jouin@gmail.com>
2018-07-03 13:53:09 +0200

    '0.1.02'

--- maj.py
+++ maj.py
@@ -5,9 +5,9 @@ update_pypi = ['python3','setup.py','sdist','bdist_wheel']
 git_add     = ['git','add',  '.']
 git_commit  = ['git','commit', '-m', "\'{version}\'".format(version=jr_data_science.__version__)]
 git_push    = ['git', 'push', 'origin']
-#twine       = ['twine', 'upload', '--skip-existing', 'dist/*']
+twine       = ['twine', 'upload', '--skip-existing', 'dist/*']
 
-commands = [update_pypi, git_add, git_commit,  git_push,]# twine]
+commands = [update_pypi, git_add, git_commit,  git_push, twine]
 for bash_command in commands :
     print ("== =="*10)
     print(" ".join(bash_command))
