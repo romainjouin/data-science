f45cfdffa754fce24946dcdea1c81f317faa73e8
Romain Jouin <romain.jouin@gmail.com>
2018-07-03 18:13:30 +0200

    '0.1.2.4'

--- maj.py
+++ maj.py
@@ -6,13 +6,14 @@ git_add     = ['git','add',  '.']
 git_commit  = ['git','commit', '-m', "\'{version}\'".format(version=jr_data_science.__version__)]
 git_push    = ['git', 'push', 'origin']
 twine       = ['twine', 'upload', '--skip-existing', 'dist/*']
-
-commands = [update_pypi, git_add, git_commit,  git_push, twine]
+pip_upgrade = ['pip','install','--ignore-installed','--no-cache-dir','jr_data_science']
+commands = [update_pypi, git_add, git_commit,  git_push, twine, pip_upgrade ]
 for bash_command in commands :
     print ("== =="*10)
     print(" ".join(bash_command))
     print ("== ==" * 10)
-
+    if bash_command == twine:
+        print("user ?")
     process = subprocess.Popen(bash_command, stdout=subprocess.PIPE)
     output, error = process.communicate()
     print("output = ")
